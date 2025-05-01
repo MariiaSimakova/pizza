a6935f2391c4d408542e228408b0d636211ba456
MariiaSimakova <mariiasimakova6@gmail.com>
2025-05-01 15:25:32 +1000

    add Skeleton when loading the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
-import PizzaBlock from "./components/PizzaBlock";
+import PizzaBlock from "./components/PizzaBlock/PizzaBlock";
+import Skeleton from "./components/PizzaBlock/Skeleton";
 
 import { useEffect, useState } from "react";
 
@@ -9,12 +10,14 @@ import "../scss/app.scss";
 
 function App() {
   const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://6811c5a73ac96f7119a58412.mockapi.io/items")
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr);
+        setIsLoading(false);
       });
   }, []);
 
@@ -30,9 +33,12 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              {items.map((obj) => (
-                <PizzaBlock key={obj.id} {...obj} />
-              ))}
+              {/* {items.map((obj) =>
+                isLoading ? <Skeleton /> : <PizzaBlock key={obj.id} {...obj} />
+              )} */}
+              {isLoading
+                ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+                : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
             </div>
           </div>
         </div>
