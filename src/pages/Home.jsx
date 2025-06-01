d3d2a3becab77d8a37ff06efda39ff658e933fea
MariiaSimakova <mariiasimakova6@gmail.com>
2025-06-01 17:50:59 +1000

    add Pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { Pagination } from "@mui/material";
+
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { setCategoryId } from "../redux/slices/filterSlice";
@@ -18,6 +20,14 @@ const Home = ({ searchValue }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [orderType, setOrderType] = useState(true);
 
+  // current page
+  const [page, setPage] = useState(1);
+  //Pizza total cause MockApi doesn give you the number for free
+  const pizzaSum = 10;
+  const limit = 4;
+  // Общее количество страниц для пагинации
+  const pageSum = Math.ceil(pizzaSum / limit);
+
   const onChangeCategory = (id) => {
     dispatch(setCategoryId(id));
   };
@@ -26,16 +36,17 @@ const Home = ({ searchValue }) => {
     setIsLoading(true);
     axios
       .get(
-        `https://6811c5a73ac96f7119a58412.mockapi.io/items?category=${categoryId}&sortBy=${sortType}&order=${
+        `https://6811c5a73ac96f7119a58412.mockapi.io/items?page=${page}&limit=${limit}&category=${categoryId}&sortBy=${sortType}&order=${
           orderType ? "desc" : "asc"
-        }`
+        }
+        `
       )
       .then((res) => {
         setItems(res.data);
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType, orderType, searchValue]);
+  }, [categoryId, sortType, orderType, page]);
 
   const pizzas = items
     .filter((obj) => {
@@ -57,6 +68,13 @@ const Home = ({ searchValue }) => {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">{isLoading ? skeleton : pizzas}</div>
+
+      <Pagination
+        count={pageSum}
+        page={page}
+        onChange={(_, num) => setPage(num)}
+        size="large"
+      />
     </div>
   );
 };
