b8a4e7c724ac54b81ed6bffd0985509d250b650f
MariiaSimakova <mariiasimakova6@gmail.com>
2025-06-10 17:00:37 +1000

    add cart slice

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import logoSvg from "../assets/img/pizza-logo.svg";
 import Search from "./Search/Search";
 
 function Header({ searchValue, setSearchValue }) {
+  // вернем весь карт сюда
+  const { items, totalPrice } = useSelector((state) => state.cart);
   return (
     <div className="header">
       <div className="container">
@@ -18,7 +21,7 @@ function Header({ searchValue, setSearchValue }) {
         <Search searchValue={searchValue} setSearchValue={setSearchValue} />
         <div className="header__cart">
           <Link to="/cart" className="button button--cart">
-            <span>520 ₽</span>
+            <span>{totalPrice} ₽</span>
             <div className="button__delimiter"></div>
             <svg
               width="18"
@@ -49,7 +52,7 @@ function Header({ searchValue, setSearchValue }) {
                 strokeLinejoin="round"
               />
             </svg>
-            <span>3</span>
+            <span>{items.length}</span>
           </Link>
         </div>
       </div>
